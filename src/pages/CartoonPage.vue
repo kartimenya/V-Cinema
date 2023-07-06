fbd9617057030b1bf024b17ca6774410ca9daab9
Your Name <you@example.com>
2023-07-06 21:25:28 +0300

    pages

---
+++ src/pages/CartoonPage.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import FilmCard from '@/components/FilmCard.vue';
+import { useMovieStore } from '@/stores/MovieStore';
+
+const { movies } = useMovieStore();
+</script>
+
+<template>
+  <section>
+    <h1>Фильмы</h1>
+    <div class="films">
+      <FilmCard v-for="movie in movies" :key="movie.imdbId" :movie="movie" />
+    </div>
+  </section>
+</template>
+
+<style>
+.films {
+  display: grid;
+  grid-gap: 20px;
+  gap: 20px;
+  grid-template-columns: repeat(6, 1fr);
+}
+</style>
