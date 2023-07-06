5e8b6b56ad87fe698a7668077d45b089af3bccbb
Your Name <you@example.com>
2023-07-06 21:26:02 +0300

    init movieStore

---
+++ src/stores/MovieStore.ts
@@ -0,0 +1,31 @@
+import type { IMovie } from '@/models/movie';
+import axios from 'axios';
+import { defineStore } from 'pinia';
+
+export const useMovieStore = defineStore('movieStore', {
+  state: () => ({
+    movies: [] as IMovie[],
+    isLoading: false,
+  }),
+  actions: {
+    async getMovies() {
+      try {
+        this.isLoading = true;
+        const { data } = await axios.get(
+          'https://api.kinopoisk.dev/v1.3/movie?type=movie&limit=42',
+          {
+            headers: {
+              'X-API-KEY': 'AV16W7K-PQ0436M-H7WH58W-7EQ1W7E',
+            },
+          },
+        );
+        this.movies = data.docs;
+        this.isLoading = false;
+      } catch (error) {
+        console.error(error);
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
+});
