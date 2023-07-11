5ba950abc6db90c854dc6e12d41aad26f2508f07
Your Name <you@example.com>
2023-07-11 21:47:46 +0300

    interface for FullMovie

--- src/models/movie.ts
+++ src/models/movie.ts
@@ -11,3 +11,25 @@ export interface IMovie {
     previewUrl: string;
   };
 }
+
+export interface IMovieFull extends IMovie {
+  ageRating: number;
+  backdrop: {
+    url: string;
+  };
+  countries: {
+    name: string;
+  }[];
+  genres: { name: string }[];
+  logo: {
+    url: string;
+  };
+  shortDescription: string;
+  persons: IPersons[];
+}
+
+interface IPersons {
+  name: string;
+  photo: string;
+  profession: 'режиссеры' | 'актеры';
+}
