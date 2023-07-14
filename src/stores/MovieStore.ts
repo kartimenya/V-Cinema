accaa3ce1a097aca87183db277255ba0ea2b4437
Your Name <you@example.com>
2023-07-14 19:38:46 +0300

    getMoreMovies function

--- src/stores/MovieStore.ts
+++ src/stores/MovieStore.ts
@@ -2,19 +2,29 @@ import type { IMovie } from '@/models/movie';
 import axios from 'axios';
 import { defineStore } from 'pinia';
 
+interface GetMoviesResponse {
+  docs: IMovie[];
+  page: number;
+  pages: number;
+  total: number;
+  limit: number;
+}
+
 export const useMovieStore = defineStore('movieStore', {
   state: () => ({
     movies: [] as IMovie[],
     isLoading: false,
+    focusPage: 4,
+    pages: 5,
   }),
   actions: {
-    async getMovies(type: string, genre?: string) {
+    async getMovies(type: string, genre?: string, rating?: string) {
       try {
         this.isLoading = true;
-        const { data } = await axios.get(
-          `https://api.kinopoisk.dev/v1.3/movie?type=${type}&limit=42&${
-            genre ? 'genres.name=' + genre : ''
-          }`,
+        const { data } = await axios.get<GetMoviesResponse>(
+          `https://api.kinopoisk.dev/v1.3/movie?type=${type}&${
+            rating ? 'rating.imdb=' + rating : ''
+          }&${genre ? 'genres.name=' + genre : ''}&limit=42`,
           {
             headers: {
               'X-API-KEY': 'AV16W7K-PQ0436M-H7WH58W-7EQ1W7E',
@@ -22,11 +32,33 @@ export const useMovieStore = defineStore('movieStore', {
           },
         );
         this.movies = data.docs;
-        this.isLoading = false;
       } catch (error) {
         console.error(error);
       } finally {
         this.isLoading = false;
+      }
+    },
+    async getMoreMovies(type: string, genre?: string, rating?: string) {
+      if (this.focusPage < this.pages) {
+        try {
+          const { data } = await axios.get<GetMoviesResponse>(
+            `https://api.kinopoisk.dev/v1.3/movie?type=${type}&${
+              rating ? 'rating.imdb=' + rating : ''
+            }&${genre ? 'genres.name=' + genre : ''}&limit=18&page=${this.focusPage}`,
+            {
+              headers: {
+                'X-API-KEY': 'AV16W7K-PQ0436M-H7WH58W-7EQ1W7E',
+              },
+            },
+          );
+          this.focusPage++;
+          this.movies = [...this.movies, ...data.docs];
+          this.pages = data.pages;
+        } catch (error) {
+          console.error(error);
+        } finally {
+          this.isLoading = false;
+        }
       }
     },
   },
